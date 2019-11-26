8921d7b6d0504b79a1de64f3e4ee6062d6c03622
LukeHD <31736829+LukeHD@users.noreply.github.com>
2019-11-26 20:19:46 +0100

    added further ajax and introduces first popups (-> need styling)

--- blog.py
+++ blog.py
@@ -8,9 +8,11 @@ import os
 from flask_login import UserMixin
 from werkzeug.security import generate_password_hash, check_password_hash
 from werkzeug.urls import url_parse
+from werkzeug.datastructures import MultiDict
 from wtforms.validators import ValidationError
 
 from flask_wtf import FlaskForm
+from flask_wtf.csrf import CSRFProtect
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
 from wtforms.validators import ValidationError, DataRequired, Email, EqualTo
 
@@ -21,9 +23,13 @@ app.config.update(
     SECRET_KEY = '/x83j/xe7/x97/x9e///xf1/x17/xca/xd2/xde/x8f/xa9S/xca/xce/xad/x7f}/x03/x9d{/x14/xfe/x9b/xb1$/x143/xd5n~'
 )
 db = SQLAlchemy(app)
+csrf = CSRFProtect()
+csrf.init_app(app)
 migrate = Migrate(app, db)
 login = LoginManager(app)
 login.login_view = 'login'
+
+current_error = ""
 
 class Blog_Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -136,7 +142,7 @@ def manage_admin(user_id):
                 db.session.commit()
                 return redirect(url_for('admin'))
             else:
-                raise Exception("You're not allowed to change your own admin-status")
+                raise Exception("Sie können nicht ihren eigenen Admin-Status verändern")
         except Exception as e: 
                 return render_template("error.html", title="Error", error=e)
     else:
@@ -148,20 +154,22 @@ def page_not_found(e):
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-    form = LoginForm()
-    if current_user.is_authenticated:
-        return redirect(url_for('index'))
-    if form.validate_on_submit():
-        user = User.query.filter_by(username=form.username.data).first()
-        if user is None or not user.check_password(form.password.data):
-            return redirect("javascript:popup()")
-        # TODO: add message logged in
-        login_user(user, remember=form.remember_me.data)
-        next_page = request.args.get('next')
-        if not next_page or url_parse(next_page).netloc != '':
-            next_page = url_for('index')
-        return redirect(next_page)
-    return render_template('login.html', title='Login', form=form)
+    if request.method == 'GET':
+        form = LoginForm()
+        if current_user.is_authenticated:
+            return redirect(url_for('index'))
+        return render_template('login.html', title='Login', form=form)
+    elif request.method == 'POST':
+        form = request.form
+        user = User.query.filter_by(username=form['username'].strip()).first()
+        if user is None or not user.check_password(form['password'].strip()):
+            return jsonify({"text": "Benutzername oder Passwort ist falsch.", "redirect": False})
+        else: 
+            login_user(user, remember=form['remember_me'])
+            next_page = request.args.get('next')
+            if not next_page or url_parse(next_page).netloc != '':
+                next_page = url_for('index')
+            return jsonify({"text": next_page, "redirect": True})
 
 @app.route('/logout')
 @login_required
@@ -171,46 +179,36 @@ def logout():
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
-    if current_user.is_authenticated:
-        return redirect(url_for('index'))
-    form = RegistrationForm()
-    if form.validate_on_submit():
-        user = User(username=form.username.data, email=form.email.data)
-        user.set_password(form.password.data)
-        db.session.add(user)
-        db.session.commit()
-        # TODO: add message registered
-        return redirect(url_for('login'))
-    return render_template('register.html', title='Register', form=form)
-
+    if request.method == 'GET':
+        form = RegistrationForm()
+        if current_user.is_authenticated:
+            return redirect(url_for('index'))
+        return render_template('register.html', title='Register', form=form)
+    elif request.method == 'POST':
+        form = request.form
+        if User.query.filter_by(username=form['username'].strip()).first() is not None:
+            return jsonify({"text": "Bitte wählen Sie einen anderen Benutzernamen.", "redirect": False})
+        elif User.query.filter_by(email=form['email'].strip()).first() is not None:
+            return jsonify({"text": "Bitte wählen Sie eine andere Email-Addresse.", "redirect": False})
+        else: 
+            user = User(username=form['username'].strip(), email=form['email'].strip())
+            user.set_password(form['password'].strip())
+            db.session.add(user)
+            db.session.commit()
+            return jsonify({"text": url_for('login'), "redirect": True})
+                
 class LoginForm(FlaskForm):
-    username = StringField('Username', validators=[DataRequired()], render_kw={"placeholder": "Username"})
-    password = PasswordField('Password', validators=[DataRequired()], render_kw={"placeholder": "Password"})
-    remember_me = BooleanField('Remember Me')
+    username = StringField(validators=[DataRequired()], render_kw={"placeholder": "Benutzername"})
+    password = PasswordField(validators=[DataRequired()], render_kw={"placeholder": "Passwort"})
+    remember_me = BooleanField('Angemeldet bleiben')
     submit = SubmitField('Login')
 
 class RegistrationForm(FlaskForm):
-    username = StringField(validators=[DataRequired()], render_kw={"placeholder": "Username"})
+    username = StringField(validators=[DataRequired()], render_kw={"placeholder": "Benutzername"})
     email = StringField(validators=[DataRequired(), Email()], render_kw={"placeholder": "Email"})
-    password = PasswordField(validators=[DataRequired()], render_kw={"placeholder": "Password"})
-    password2 = PasswordField(validators=[DataRequired(), EqualTo('password')], render_kw={"placeholder": "Repeat Password"})
+    password = PasswordField(validators=[DataRequired()], render_kw={"placeholder": "Passwort"})
+    password2 = PasswordField(validators=[DataRequired(), EqualTo('password')], render_kw={"placeholder": "Passwort wiederholen"})
     submit = SubmitField('Register')
-
-    def validate_username(self, username):
-        user = User.query.filter_by(username=username.data).first()
-        if user is not None:
-            # TODO: warning: different username
-            raise ValidationError('Please use a different username.')
-
-    def validate_email(self, email):
-        user = User.query.filter_by(email=email.data).first()
-        if user is not None:
-            # TODO: warning: different email
-            raise ValidationError('Please use a different email address.')
-
-@app.route("/popup", methods=['POST'])
-def popup_json():
-    return jsonify({"text": "hallo"})
 
 if __name__ == "__main__":
     db.create_all()
