8464e6a979d5305cd7ec299f5f9512577c2d7c10
LukeHD <31736829+LukeHD@users.noreply.github.com>
2019-12-12 23:56:45 +0100

    Fixed line break

--- blog.py
+++ blog.py
@@ -11,8 +11,7 @@ app = Flask(__name__)
 app.config.update(
     SQLALCHEMY_DATABASE_URI='sqlite:///blog_posts.db',
     DEBUG=True,
-    SECRET_KEY=
-    '/x83j/xe7/x97/x9e///xf1/x17/xca/xd2/xde/x8f/xa9S/xca/xce/xad/x7f}/x03/x9d{/x14/xfe/x9b/xb1$/x143/xd5n~',
+    SECRET_KEY='/x83j/xe7/x97/x9e///xf1/x17/xca/xd2/xde/x8f/xa9S/xca/xce/xad/x7f}/x03/x9d{/x14/xfe/x9b/xb1$/x143/xd5n~',
     SQLALCHEMY_TRACK_MODIFICATIONS=False)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
